bfb33a9585e80630eeb4241af8ff618c66bbdae0
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-11-14 16:19:18 -0800

    adds mern logo to about me

--- src/components/AboutMeCard.js
+++ src/components/AboutMeCard.js
@@ -1,13 +1,15 @@
 import Card from 'react-bootstrap/Card';
+import MERNLogo from '../MERNLogo.png';
 
 function AboutMeCard() {
   return (
     <Card>
       <Card.Header>About Me</Card.Header>
+      <Card.Img variant="top" src={MERNLogo} className="mernLogo mx-auto" />
       <Card.Body>
-        <Card.Title>My Elevator Pitch</Card.Title>
+      
         <Card.Text>
-        Digital Forensics Examiner with the King County Sheriff's Office. Full Stack JavaScript Web Developer, MERN Stack. CompTIA S+, N+, A+ Certified. USSS Cyber Fraud Task Force Member. 
+        I am a Digital Forensics Examiner with the King County Sheriff's Office and a Full Stack JavaScript Web Developer, MERN Stack. CompTIA S+, N+, A+ Certified. USSS Cyber Fraud Task Force Member. 
 
  I've worked to specialize my career toward the investigation of digital crimes to merge my education, interests, and passions of technology and psychology to help people. I am a Security Researcher and I spend my free time developing my Information Security and Web Development skills.
         
