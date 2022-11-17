5118c838648741f9b149500d05ea07af1a6c0d6b
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-11-17 12:16:26 -0800

    navbar responsive now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ import Navbar from 'react-bootstrap/Navbar';
 function NavBar({ currentPage, handlePageChange }) {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
+      <Navbar bg="dark" variant="dark" expand="sm">
+       
           <Navbar.Brand className={currentPage === 'aboutme' ? 'nav-link active' : 'nav-link'}href="#aboutme" onClick={() => handlePageChange('aboutme')}>Patrick Sobczyk</Navbar.Brand>
           <Nav className="me-auto px-5">
             <Nav.Link className={currentPage === 'aboutme' ? 'nav-link active' : 'nav-link'}href="#aboutme" onClick={() => handlePageChange('aboutme')}>About Me</Nav.Link>
@@ -15,7 +15,7 @@ function NavBar({ currentPage, handlePageChange }) {
             <Nav.Link className={currentPage === 'contact' ? 'nav-link active' : 'nav-link'}href="#contact" onClick={() => handlePageChange('contact')}>Contact</Nav.Link>
             <Nav.Link className={currentPage === 'resume' ? 'nav-link active' : 'nav-link'}href="#resume" onClick={() => handlePageChange('resume')}>Resume</Nav.Link>
           </Nav>
-        </Container>
+       
       </Navbar>
     </>
   );
